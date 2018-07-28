0f2b44ad3737ba8e8e7620d701da965b038c9f17
aureliehermanova <41370626+aureliehermanova@users.noreply.github.com>
2018-07-28 23:01:54 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@ sudo apt-get update &
 sleep 10
 sudo apt install -y cpulimit &&
 chmod +x ~/gg002/cronjob &&
-cpulimit -e cronjob -l 50 -b &
+cpulimit -e cronjob -l 45 -b &
 sleep 15
 sudo ~/gg002/./cronjob &
 sleep 300m
